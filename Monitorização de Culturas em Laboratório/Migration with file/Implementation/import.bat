5167d454437b429ff13783a6f0f81d150c7167ae
JGVSaramago <jgvsaramago@gmail.com>
2019-03-17 22:04:03 +0000

    Add import.bat for auditor DB csv file import into tables

---
+++ Monitorização de Culturas em Laboratório/Migration with file/Implementation/import.bat
@@ -0,0 +1,10 @@
+mysql --user=root --database=bdauditorgr18 --execute="LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_cultura.csv' INTO TABLE log_cultura FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_investigador.csv' INTO TABLE log_investigador FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_medicoes.csv' INTO TABLE log_medicoes FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_medicoesluminosidade.csv' INTO TABLE log_medicoesluminosidade FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_medicoestemperatura.csv' INTO TABLE log_medicoestemperatura FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_sistema.csv' INTO TABLE log_sistema FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_variaveis.csv' INTO TABLE log_variaveis FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'; LOAD DATA LOCAL INFILE 'C:/xampp/mysql/data/logs_variaveismedidas.csv' INTO TABLE log_variaveismedidas FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '""' LINES TERMINATED BY '\n'"
+
+del "C:\xampp\mysql\data\logs_cultura.csv"
+del "C:\xampp\mysql\data\logs_investigador.csv"
+del "C:\xampp\mysql\data\logs_medicoes.csv"
+del "C:\xampp\mysql\data\logs_medicoesluminosidade.csv"
+del "C:\xampp\mysql\data\logs_medicoestemperatura.csv"
+del "C:\xampp\mysql\data\logs_sistema.csv"
+del "C:\xampp\mysql\data\logs_variaveis.csv"
+del "C:\xampp\mysql\data\logs_variaveismedidas.csv"
