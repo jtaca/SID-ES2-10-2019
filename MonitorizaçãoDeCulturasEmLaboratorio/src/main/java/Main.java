d384fb977a0cb8afe5d5194177751e0c09d6d29e
JGVSaramago <jgvsaramago@gmail.com>
2019-04-04 20:52:33 +0100

    Fix main class in lowercase; Fix DatabaseConnection not connecting; Fix result from select in DatabaseConnection not being possible to be read outside the select function; Add examples of DB connection and select usage

---
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/Main.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import main.java.api.DatabaseConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Starting app...");
+
+        DatabaseConnection db = new DatabaseConnection();
+
+        System.out.println("\nTabela de logs\n");
+        ResultSet result = db.select("SELECT * FROM logs");
+        while (result.next()) {
+            String logid = result.getString(1);
+            System.out.println("LogID: " + logid);
+        }
+
+        System.out.println("\nTabela de investigadores\n");
+        result = db.select("SELECT * FROM investigador");
+        while (result.next()) {
+            String email = result.getString(1);
+            String name = result.getString(2);
+            String prof = result.getString(3);
+            System.out.println("Email: " + email + " | Nome: " + name + " | Categoria: " + prof);
+        }
+    }
+
+}
--- MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/api/DatabaseConnection.java
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/api/DatabaseConnection.java
@@ -7,30 +7,30 @@ import java.sql.ResultSet;
 
 
 public class DatabaseConnection {
-	
+
 	private static Connection conn = null;
 
 	public DatabaseConnection() {
-		
-		// vou meter isto bonitinho em termos de extrutura � s� mm para poderem trabalhar
-		
-		try {
-		    conn =
-		       DriverManager.getConnection("jdbc:mysql://localhost/estufa?" +
-		                                   "user=estufa&password="); // mudar para phpUser maybe?
 
-		    // Do something with the Connection
+		// vou meter isto bonitinho em termos de extrutura � s� mm para poderem trabalhar
+
+		try {
+			conn =
+					DriverManager.getConnection("jdbc:mysql://localhost/estufa?&serverTimezone=UTC&" +
+							"user=root&password="); // mudar para phpUser maybe?
+
+			// Do something with the Connection
 
 		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 
 	}
-	
-	public static ResultSet select(String query) {
+
+	public ResultSet select(String query) {
 
 		// assume that conn is an already created JDBC connection (see previous examples)
 
@@ -38,52 +38,76 @@ public class DatabaseConnection {
 		ResultSet rs = null;
 
 		try {
-		    stmt = conn.createStatement();
-		    rs = stmt.executeQuery(query);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
 
-		    // or alternatively, if you don't know ahead of time that
-		    // the query will be a SELECT...
-
-		    if (stmt.execute(query)) {
-		        rs = stmt.getResultSet();
-		        return rs;
-		    }
-
-		    // Now do something with the ResultSet ....
 		}
 		catch (SQLException ex){
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-		finally {
-		    // it is a good idea to release
-		    // resources in a finally{} block
-		    // in reverse-order of their creation
-		    // if they are no-longer needed
 
-		    if (rs != null) {
-		        try {
-		            rs.close();
-		        } catch (SQLException sqlEx) { } // ignore
-
-		        rs = null;
-		    }
-
-		    if (stmt != null) {
-		        try {
-		            stmt.close();
-		        } catch (SQLException sqlEx) { } // ignore
-
-		        stmt = null;
-		    }
-		}
 		return rs;
-		
-		
-		
 	}
-	
+
+
+//	public static ResultSet select(String query) {
+//
+//		// assume that conn is an already created JDBC connection (see previous examples)
+//
+//		Statement stmt = null;
+//		ResultSet rs = null;
+//
+//		try {
+//		    stmt = conn.createStatement();
+//		    rs = stmt.executeQuery(query);
+//			while (rs.next()) {
+//				String coffeeName = rs.getString(1);
+//				System.out.println(coffeeName);
+//			}
+//		    // or alternatively, if you don't know ahead of time that
+//		    // the query will be a SELECT...
+//
+//		    if (stmt.execute(query)) {
+//		        rs = stmt.getResultSet();
+//
+//		        return rs;
+//		    }
+//
+//		    // Now do something with the ResultSet ....
+//		}
+//		catch (SQLException ex){
+//		    // handle any errors
+//		    System.out.println("SQLException: " + ex.getMessage());
+//		    System.out.println("SQLState: " + ex.getSQLState());
+//		    System.out.println("VendorError: " + ex.getErrorCode());
+//		}
+//		finally {
+//		    // it is a good idea to release
+//		    // resources in a finally{} block
+//		    // in reverse-order of their creation
+//		    // if they are no-longer needed
+//
+//		    if (rs != null) {
+//		        try {
+//		            rs.close();
+//		        } catch (SQLException sqlEx) { } // ignore
+//
+//		        rs = null;
+//		    }
+//
+//		    if (stmt != null) {
+//		        try {
+//		            stmt.close();
+//		        } catch (SQLException sqlEx) { } // ignore
+//
+//		        stmt = null;
+//		    }
+//		}
+//		return rs;
+//	}
+
 
 }
