8f52fc5b1b82c5fe8fdf8258b20278e264575b0a
JGVSaramago <jgvsaramago@gmail.com>
2019-04-04 19:46:06 +0100

    Change project structure to Maven Standard Directory Layout

---
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/api/DatabaseConnection.java
@@ -0,0 +1,89 @@
+package api;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
+public class DatabaseConnection {
+	
+	private static Connection conn = null;
+
+	public DatabaseConnection() {
+		
+		// vou meter isto bonitinho em termos de extrutura é só mm para poderem trabalhar
+		
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://localhost/estufa?" +
+		                                   "user=estufa&password="); // mudar para phpUser maybe?
+
+		    // Do something with the Connection
+
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+
+	}
+	
+	public static ResultSet select(String query) {
+
+		// assume that conn is an already created JDBC connection (see previous examples)
+
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery(query);
+
+		    // or alternatively, if you don't know ahead of time that
+		    // the query will be a SELECT...
+
+		    if (stmt.execute(query)) {
+		        rs = stmt.getResultSet();
+		        return rs;
+		    }
+
+		    // Now do something with the ResultSet ....
+		}
+		catch (SQLException ex){
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		finally {
+		    // it is a good idea to release
+		    // resources in a finally{} block
+		    // in reverse-order of their creation
+		    // if they are no-longer needed
+
+		    if (rs != null) {
+		        try {
+		            rs.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        rs = null;
+		    }
+
+		    if (stmt != null) {
+		        try {
+		            stmt.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        stmt = null;
+		    }
+		}
+		return rs;
+		
+		
+		
+	}
+	
+
+}
