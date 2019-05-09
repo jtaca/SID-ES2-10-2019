ccd4b03054a97bbfd36880527b3830bbed5b0969
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2019-05-09 13:55:06 +0100

    Cultura tests created

---
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/cultura_tests/CultureTest.java
@@ -0,0 +1,54 @@
+package cultura_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CultureTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCultureIntegerStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCultureStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCultureName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCultureDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInvestigatorEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
