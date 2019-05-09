35feb492994f689a22770485f357558db6cf7c6e
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2019-05-09 14:00:14 +0100

    Medicoes tests created

---
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/medicoes_tests/MedicoesTest.java
@@ -0,0 +1,54 @@
+package medicoes_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MedicoesTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testMedicoesIntegerStringDoubleInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMedicoesDoubleInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumeroMedicao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDataHoraMedicao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetValorMedicao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetIdVariaveisMedidas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
