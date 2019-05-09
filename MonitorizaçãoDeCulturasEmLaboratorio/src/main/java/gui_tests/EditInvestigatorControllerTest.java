18f713285d85b6224f06e8cfc95fbfc1162085cd
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2019-05-09 13:58:39 +0100

    GUI tests created

---
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/gui_tests/EditInvestigatorControllerTest.java
@@ -0,0 +1,24 @@
+package gui_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EditInvestigatorControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRegister() {
+		fail("Not yet implemented");
+	}
+
+}
