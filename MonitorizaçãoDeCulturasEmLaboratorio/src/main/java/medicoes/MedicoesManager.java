45483190954dc809901b749b319c3c867b0662b5
Joao Tiago Aparicio <joaotiagoaparicio@gmail.com>
2019-05-12 13:19:30 +0100

    Select Medicoes updated for cultura argument

--- MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/medicoes/MedicoesManager.java
+++ MonitorizaçãoDeCulturasEmLaboratorio/src/main/java/medicoes/MedicoesManager.java
@@ -1,7 +1,11 @@
 package medicoes;
 
 import api.DatabaseConnection;
+import cultura.Culture;
+
 import com.mysql.cj.jdbc.CallableStatement;
+
+import java.awt.*;
 import java.util.List;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,134 +18,133 @@ import java.util.ArrayList;
 
 public class MedicoesManager {
 
-  private final String TABELA_CULTURA = "medicoes";
-  private List<Medicoes> listOfMedicoes = new ArrayList<Medicoes>();
-
+    private final String TABELA_CULTURA = "medicoes";
+    private List<Medicoes> listOfMedicoes = new ArrayList<Medicoes>();
 
     /**
      * @return an ArrayList of all the measures in the database;
      */
 
-  public List<Medicoes> getListOfMedicoes() {
-      return listOfMedicoes;
-  }
+    public List<Medicoes> getListOfMedicoes() {
+        return listOfMedicoes;
+    }
 
     /**
      * Extracts all records from the medicoes table measurements from the database.
      */
 
-  public void getDBMedicoes() {
+    public void getDBMedicoes() {
 
-      DatabaseConnection DB = DatabaseConnection.getInstance();
+        DatabaseConnection DB = DatabaseConnection.getInstance();
 
-      if(DB.isConnected()) {
-          listOfMedicoes.clear();
-          ResultSet medicoesResultSet = DB.select("SELECT * FROM estufa.medicoes");
-          try {
-              addMedicoesResultSet(medicoesResultSet);
-          } catch (SQLException sqlException) {
-              sqlException.printStackTrace();
-          }
-      }
-  }
+        if(DB.isConnected()) {
+            listOfMedicoes.clear();
+            ResultSet medicoesResultSet = DB.select("SELECT * FROM estufa.medicoes");
+            try {
+
+                addMedicoes(extractMedicoes(medicoesResultSet));
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        }
+    }
+
+    private List<Medicoes> extractMedicoes (ResultSet varMedicao) throws SQLException {
+        List<Medicoes> AuxlistOfMedicoes = new ArrayList<Medicoes>();
+        while(varMedicao.next()) {
+            int numeroMedicao = varMedicao.getInt("NumeroMedicao");
+            String dataHoraMedicao = varMedicao.getString("DataHoraMedicao");
+            double valorMedicao = varMedicao.getDouble("ValorMedicao");
+            int idVariaveisMedidas = varMedicao.getInt("idVariaveisMedidas");
+
+            AuxlistOfMedicoes.add(new Medicoes(numeroMedicao, dataHoraMedicao,
+                    valorMedicao, idVariaveisMedidas));
+
+        }
+        return AuxlistOfMedicoes;
+    }
 
     /**
      * Transforms all the records obtained in the data base`s medicoes table in objects medicoes. It inserts those objects in a medicoes list.
      * @param varMedicao represents the record of a medicao extrated from the data base for a post transformation.
      */
 
-  private void addMedicoesResultSet(ResultSet varMedicao) throws SQLException {
-
-      while(varMedicao.next()) {
-          int numeroMedicao = varMedicao.getInt("NumeroMedicao");
-          String dataHoraMedicao = varMedicao.getString("DataHoraMedicao");
-          double valorMedicao = varMedicao.getDouble("ValorMedicao");
-          int idVariaveisMedidas = varMedicao.getInt("idVariaveisMedidas");
-
-          listOfMedicoes.add(new Medicoes(numeroMedicao, dataHoraMedicao, valorMedicao, idVariaveisMedidas));
-
-      }
-  }
+    private void addMedicoes (List<Medicoes> extracted) {
+        for (Medicoes medicoes : extracted) {
+            listOfMedicoes.add(medicoes);
+        }
+    }
 
     /**
      * Insert a new measure into the database. If the measure does not have an ID it will be assigned one automatically by the database auto-increment.
      * @param medicao the measure to be inserted.
      */
-  public void insertMedicoes (Medicoes medicao) {
 
-      DatabaseConnection DB = DatabaseConnection.getInstance();
+    public void insertMedicoes (Medicoes medicao) {
 
-      if(DB.isConnected()) {
-          DB.insert(TABELA_CULTURA, medicao.toString());
-      }
+        DatabaseConnection DB = DatabaseConnection.getInstance();
 
-      getDBMedicoes();
-  }
+        if(DB.isConnected()) {
+            DB.insert(TABELA_CULTURA, medicao.toString());
+        }
 
-
-  public void updateMedicoes (Medicoes oldMedicao, Medicoes newMedicao) {
-
-      try {
-
-          CallableStatement cStmt = (CallableStatement) DatabaseConnection.getInstance().getConnection().prepareCall("{call alterarValorMedido(?,?)}");
-          cStmt.setString(1, ""+oldMedicao.getNumeroMedicao());
-          cStmt.setString(2, ""+newMedicao.getValorMedicao());
-
-          if(cStmt.execute()==false) {
-              System.out.println("A medição com id: " + oldMedicao.getNumeroMedicao() + " foi atualizada com sucesso.");
-          }
-      } catch (SQLException e) {
-          System.out.println("Nao foi possível atualizar a medição pretendida. Exception: " + e.getMessage());
-      }
-
-      getDBMedicoes();
-  }
-
+        getDBMedicoes();
+    }
 
     /**
      * Delete a measure into the database.
      * @param medicao the measure to be deleted.
      */
 
-  public void deleteMedicoes (Medicoes medicao) {
 
-      try {
-          CallableStatement cStmt = (CallableStatement) DatabaseConnection.getInstance().getConnection().prepareCall
-                  ("{call apagarMedicao(?)}");
-          cStmt.setString(1, ""+medicao.getNumeroMedicao());
+    public void deleteMedicoes (Medicoes medicao) {
 
-          if(cStmt.execute() == false) {
-              System.out.println("A medição com número: " + medicao.getNumeroMedicao() + " foi eliminada com sucesso.");
-          }
-      } catch (SQLException e) {
-          System.out.println("Não foi possível apagar a medição pretendida. Exception: " + e.getMessage());
-      }
+        try {
+            CallableStatement cStmt = (CallableStatement) DatabaseConnection.getInstance().
+                    getConnection().prepareCall("{call apagarMedicao(?)}");
+            cStmt.setString(1, ""+medicao.getNumeroMedicao());
 
-      getDBMedicoes();
+            if(cStmt.execute() == false) {
+                System.out.println("A medicao com numero: " + medicao.getNumeroMedicao() +
+                        " foi eliminada com sucesso.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel apagar a medicao pretendida. Exception: " + e.getMessage());
+        }
 
-  }
+        getDBMedicoes();
 
+    }
 
-    public void selectMedicoes() {
+    public List<Medicoes>  selectMedicoes(Culture cultura) {
+
+        DatabaseConnection DB = DatabaseConnection.getInstance();
 
         try {
             CallableStatement cStmt = (CallableStatement) DatabaseConnection.getInstance().getConnection().prepareCall
                     ("{call selectMedicoes()}");
             boolean hadResults = cStmt.execute();
-            System.out.println("Medições tem entradas? "+ hadResults);
+            System.out.println("Medicoes tem entradas? "+ hadResults);
 
+            if(DB.isConnected()) {
+                ResultSet medicoesResultSet = DB.select("Select * FROM medicoes, variaveis_medidas WHERE IDCultura = "
+                        + cultura.getId() +" and medicoes.IdVariaveisMedidas = variaveis_medidas.IdVariaveisMedidas");
+
+            }
             ResultSet rs = cStmt.getResultSet();
-            addMedicoesResultSet(rs);
 
-            //System.out.println("SelectMedicoesIn: "+ getListOfMedicoes().toString());
-
+            List<Medicoes> AuxList =  extractMedicoes(rs);
 
             if(cStmt.execute()) {
                 System.out.println("SelectMedicoes foi executado com sucesso!");
             }
+
+            return AuxList;
+
         } catch (SQLException e) {
-            System.out.println("Não foi possível executar selectMedicoes. Exception: " + e.getMessage());
+            System.out.println("Nao foi possivel executar selectMedicoes. Exception: " + e.getMessage());
         }
+        return null;
     }
 
 
